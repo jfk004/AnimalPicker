a8c30c1d51c4cbc807df2e35f1b9db1883c8a318
Fahim <fahim@Fahims-MacBook-Pro-5.local>
2025-02-19 19:42:23 -0500

    Option to choose between a cat and a dog

---
+++ AnimalPicker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AnimalPicker {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Which animal would you like to see: a dog or a cat.");
+        String choice = scan.nextLine();
+
+        if(choice.equalsIgnoreCase("dog")){
+            System.out.println("DOG ASCII ART HERE");   
+        }else {
+            System.out.println("CAT ASCII ART HERE");
+        }
+
+        scan.close();
+
+    }
+}
