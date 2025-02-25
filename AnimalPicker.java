6371093aaf567ebb9802d066160ab0ca384149d9
Fahim <fahim@Fahims-MacBook-Pro-5.local>
2025-02-25 14:46:11 -0500

    Added the cat and dog ASCII

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -11,11 +11,11 @@ public class AnimalPicker {
             choice = scan.nextLine();
 
             if(choice.equalsIgnoreCase("dog")){
-                System.out.println("DOG ASCII ART HERE");
+                System.out.println("o\'\')}____//\n `_/      )\n (_(_/-(_/");
                 break;
 
             }else if(choice.equalsIgnoreCase("cat")){
-                System.out.println("CAT ASCII ART HERE");
+                System.out.println("|\\__/,|   (`\\\n |_ _  |.--.) )\n ( T   )     /\n(((^_(((/(((_/");
                 break;
                 
             }else {
