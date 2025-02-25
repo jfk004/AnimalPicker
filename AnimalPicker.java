4955c019d03d7a5f7c03e9431a68b0c144337800
Fahim <fahim@Fahims-MacBook-Pro-5.local>
2025-02-25 14:36:57 -0500

    Continues to ask the user for a valid option if they entered an invalid option

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 public class AnimalPicker {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Which animal would you like to see: a dog or a cat.");
-        String choice = scan.nextLine();
+        
+        String choice = "";
+        
+        while(true){
+            System.out.println("Which animal would you like to see: a dog or a cat.");
+            choice = scan.nextLine();
 
-        if(choice.equalsIgnoreCase("dog")){
-            System.out.println("DOG ASCII ART HERE");   
-        }else if(choice.equalsIgnoreCase("cat")){
-            System.out.println("CAT ASCII ART HERE");
-        }else {
-            System.out.println("Invalid Option");
+            if(choice.equalsIgnoreCase("dog")){
+                System.out.println("DOG ASCII ART HERE");
+                break;
+
+            }else if(choice.equalsIgnoreCase("cat")){
+                System.out.println("CAT ASCII ART HERE");
+                break;
+                
+            }else {
+                System.err.println("Invalid Option");
+            }
         }
-
-
-        scan.close();
-
+        
     }
 }
